785ca13cdd4641e14efddc84fccf6a8d6af5a88f
Israel Buitron <israel.buitron@gmail.com>
2012-10-09 22:40:46 -0500

    Utilities methods added to Application class.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- src/mx/israelbuitron/githubjobs4a/GitHubJobsApp.java
+++ src/mx/israelbuitron/githubjobs4a/GitHubJobsApp.java
@@ -1,7 +1,39 @@
 package mx.israelbuitron.githubjobs4a;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
 
 public class GitHubJobsApp extends Application {
     public static final String TAG = "GithubJobs4A";
+
+    /**
+     * Get application version code.
+     *
+     * @param context
+     * @return Application version code.
+     */
+    public static int getAppVersionCode(Context context) {
+        try {
+            return context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), 0).versionCode;
+        } catch(NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Get application version name.
+     * 
+     * @param context
+     * @return Application version name.
+     */
+    public static String getAppVersionName(Context context) {
+        try {
+            return context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), 0).versionName;
+        } catch(final NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
