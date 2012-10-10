867cfa207a912342781f48e98e57c9329e55128d
Israel Buitron <israel.buitron@gmail.com>
2012-10-09 22:57:58 -0500

    About dialog implemented.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- src/mx/israelbuitron/githubjobs4a/AboutDialog.java
+++ src/mx/israelbuitron/githubjobs4a/AboutDialog.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
 
@@ -12,10 +14,12 @@ public class AboutDialog extends SherlockDialogFragment implements OnClickListen
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(
+                R.layout.about_dialog, null);
         AlertDialog.Builder about = new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.about_dialog_title)
                 .setIcon(android.R.drawable.ic_dialog_info)
-                .setMessage(R.string.about_dialog_text)
+                .setView(view)
                 .setNeutralButton(
                         R.string.about_dialog_accept_button_label, this);
         return about.create();
